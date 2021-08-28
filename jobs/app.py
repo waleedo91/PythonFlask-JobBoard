74f5bfd6b1b14260f9a6569dba6ee35d274b5a24
Waleed <wsaleh1991@gmail.com>
2021-08-28 13:01:37 -0500

    finished module 3

--- jobs/app.py
+++ jobs/app.py
@@ -1,4 +1,3 @@
-from sqlite3.dbapi2 import Row
 from flask import Flask, render_template, g
 import sqlite3
 
@@ -9,9 +8,9 @@ app = Flask(__name__)
 def open_connection():
     connection = getattr(g, '_connection' , None)
     if connection == None:
-        connection = sqlite3.connect(PATH)
-        g._connection = sqlite3.connect(PATH)
-        connection.row_factory = sqlite3.Row
+        connection = g._connection =  sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
 
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
