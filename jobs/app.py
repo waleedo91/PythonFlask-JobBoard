d08bf744995340f054151aecdfd0d6e87821dfd6
Waleed <wsaleh1991@gmail.com>
2021-08-28 13:21:52 -0500

    module 4 test

--- jobs/app.py
+++ jobs/app.py
@@ -32,4 +32,8 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template('index.html')
+    sql_query = '''SELECT job.id, job.title, job.description, job.salary, 
+    employer.id as employer_id, employer.name as employer_name 
+    FROM job JOIN employer ON employer.id = job.employer_id'''
+    jobs = execute_sql(sql_query)
+    return render_template('index.html', jobs=jobs)
